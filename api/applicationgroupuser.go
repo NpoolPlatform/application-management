0831bfd950e47e22e0f9bc2e0f896a9c9ce64ad7
crazyzpl <crazyzplzpl@163.com>
2021-11-09 01:55:25 +0000

    finish application group user api and test

--- api/applicationgroupuser.go
+++ api/applicationgroupuser.go
@@ -1,1 +1,40 @@
+// +build !codeanalysis
+
 package api
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/application-management/message/npool"
+	applicationgroupuser "github.com/NpoolPlatform/application-management/pkg/crud/application-group-user"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) AddGroupUsers(ctx context.Context, in *npool.AddGroupUsersRequest) (*npool.AddGroupUsersResponse, error) {
+	resp, err := applicationgroupuser.Create(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("create group error: %v", err)
+		return &npool.AddGroupUsersResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) GetGroupUsers(ctx context.Context, in *npool.GetGroupUsersRequest) (*npool.GetGroupUsersResponse, error) {
+	resp, err := applicationgroupuser.Get(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("get group users error: %v", err)
+		return &npool.GetGroupUsersResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) RemoveGroupUsers(ctx context.Context, in *npool.RemoveGroupUsersRequest) (*npool.RemoveGroupUsersResponse, error) {
+	resp, err := applicationgroupuser.Delete(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("remove group users error: %v", err)
+		return &npool.RemoveGroupUsersResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
--- api/applicationgroupuser_test.go
+++ api/applicationgroupuser_test.go
@@ -1,1 +1,145 @@
 package api
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/application-management/message/npool"
+	"github.com/go-resty/resty/v2"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApplicationGroupUserAPI(t *testing.T) { // nolint
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	cli := resty.New()
+
+	application := npool.ApplicationInfo{
+		ApplicationName:  "test-role" + uuid.New().String(),
+		ApplicationOwner: uuid.New().String(),
+	}
+
+	response := npool.CreateApplicationResponse{}
+	resp, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.CreateApplicationRequest{
+			Info: &application,
+		}).Post("http://localhost:32759/v1/create/app")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp.StatusCode())
+
+		err := json.Unmarshal(resp.Body(), &response)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, response.Info.ID, uuid.UUID{})
+			assert.Equal(t, response.Info.ApplicationName, application.ApplicationName)
+			assert.Equal(t, response.Info.ApplicationOwner, application.ApplicationOwner)
+			application.ID = response.Info.ID
+		}
+	}
+
+	groupInfo := npool.GroupInfo{
+		GroupName:  "test-group-api" + uuid.New().String(),
+		AppID:      application.ID,
+		GroupOwner: uuid.New().String(),
+	}
+
+	response1 := npool.CreateGroupResponse{}
+	resp1, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.CreateGroupRequest{
+			Info: &groupInfo,
+		}).Post("http://localhost:32759/v1/create/group")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp1.StatusCode())
+
+		err := json.Unmarshal(resp1.Body(), &response1)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, response1.Info.ID, uuid.UUID{})
+			assert.Equal(t, response1.Info.GroupName, groupInfo.GroupName)
+			assert.Equal(t, response1.Info.AppID, groupInfo.AppID)
+			assert.Equal(t, response1.Info.GroupOwner, groupInfo.GroupOwner)
+			groupInfo.ID = response1.Info.ID
+		}
+	}
+
+	appUser := npool.ApplicationUserInfo{
+		AppID:  application.ID,
+		UserID: uuid.New().String(),
+	}
+
+	response2 := npool.AddUsersToApplicationResponse{}
+	resp2, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.AddUsersToApplicationRequest{
+			AppID:   appUser.AppID,
+			UserIDs: []string{appUser.UserID},
+		}).Post("http://localhost:32759/v1/add/users/to/app")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp2.StatusCode())
+		err := json.Unmarshal(resp2.Body(), &response2)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, response2.Infos[0].ID, uuid.UUID{})
+			assert.Equal(t, response2.Infos[0].UserID, appUser.UserID)
+			assert.Equal(t, response2.Infos[0].AppID, appUser.AppID)
+			appUser.ID = response2.Infos[0].ID
+		}
+	}
+
+	groupUserInfo := npool.GroupUserInfo{
+		UserID:  appUser.UserID,
+		GroupID: groupInfo.ID,
+		AppID:   application.ID,
+	}
+
+	response3 := npool.AddGroupUsersResponse{}
+	resp3, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.AddGroupUsersRequest{
+			UserIDs: []string{groupUserInfo.UserID},
+			AppID:   groupInfo.AppID,
+			GroupID: groupUserInfo.GroupID,
+		}).Post("http://localhost:32759/v1/add/group/users")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp3.StatusCode())
+		err := json.Unmarshal(resp3.Body(), &response3)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, response3.Infos[0].ID, uuid.UUID{})
+			assert.Equal(t, response3.Infos[0].UserID, groupUserInfo.UserID)
+			assert.Equal(t, response3.Infos[0].AppID, groupUserInfo.AppID)
+			assert.Equal(t, response3.Infos[0].GroupID, groupUserInfo.GroupID)
+		}
+	}
+
+	response4 := npool.GetGroupUsersResponse{}
+	resp4, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.GetGroupUsersRequest{
+			AppID:   groupInfo.AppID,
+			GroupID: groupUserInfo.GroupID,
+		}).Post("http://localhost:32759/v1/get/group/users")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp4.StatusCode())
+		err := json.Unmarshal(resp4.Body(), &response4)
+		if assert.Nil(t, err) {
+			assert.Equal(t, response4.Infos[0].UserID, groupUserInfo.UserID)
+			assert.Equal(t, response4.Infos[0].AppID, groupUserInfo.AppID)
+			assert.Equal(t, response4.Infos[0].GroupID, groupUserInfo.GroupID)
+		}
+	}
+
+	resp5, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.RemoveGroupUsersRequest{
+			AppID:   groupUserInfo.AppID,
+			GroupID: groupUserInfo.GroupID,
+			UserIDs: []string{groupUserInfo.UserID},
+		}).Post("http://localhost:32759/v1/remove/group/users")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp5.StatusCode())
+	}
+}
